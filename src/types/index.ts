c76709b29b58487ecc071d2047be6cce96ed1847
development-hobby <jesusalcalarojas@gmail.com>
2025-01-16 22:54:06 -0400

    Add ServicePriceData interface for service pricing management

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface ServicePriceData {
+  id: string
+  measure: string
+  repair: number
+  change: number
+  rotation: number
+  disassembly: number
+  assembly: number
+  vulcanization: number
+  fineValve: number
+  thickValve: number
+  createdAt: Date
+}
