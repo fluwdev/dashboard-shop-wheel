5abef6d3e8ec6ca16f21a36f70db2be2afc838ab
development-hobby <jesusalcalarojas@gmail.com>
2025-01-19 17:35:12 -0400

    feat: wrap AppProvider with AuthProvider for authentication context

--- src/provider/app-provider.tsx
+++ src/provider/app-provider.tsx
@@ -5,6 +5,7 @@ import { SidebarProvider } from '@/components/ui/sidebar'
 import { Toaster } from '@/components/ui/toaster'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import { AuthProvider } from './auth-provider'
 
 export function AppProvider({
   children,
@@ -14,15 +15,17 @@ export function AppProvider({
   const [queryClient] = useState(new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-      >
-        <SidebarProvider>{children}</SidebarProvider>
-        <Toaster />
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider>{children}</SidebarProvider>
+          <Toaster />
+        </ThemeProvider>
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
