e35d78fc6143cd0142b15e587cdb7ca6f3b78ad1
development-hobby <jesusalcalarojas@gmail.com>
2025-01-19 17:36:06 -0400

    feat: add update and delete functionality for rubber repair services with UI integration

--- src/app/rubber-repair/page.tsx
+++ src/app/rubber-repair/page.tsx
@@ -2,13 +2,28 @@
 
 import Container from '@/components/container'
 import { SheetCreateServices } from '@/components/sheet-create-services'
+import { Button } from '@/components/ui/button'
 import { DataTable } from '@/components/ui/data-table'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet'
 import { useForm } from '@/hooks/use-form'
 import { useToast } from '@/hooks/use-toast'
 import { columnsRubberRepair } from '@/lib/columns'
-import { getRubberRepair, saveRubberRepair } from '@/services/rubber-services'
+import {
+  deleteRubberRepair,
+  getRubberRepair,
+  saveRubberRepair,
+  updateRubberRepair,
+} from '@/services/rubber-services'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
 import { RotateLoader } from 'react-spinners'
 
 const initialValues = {
@@ -21,7 +36,15 @@ const initialValues = {
 export default function RubberRepairPage() {
   const { toast } = useToast()
   const queryClient = useQueryClient()
+  const [isOpenUpdate, setIsOpenUpdate] = useState(false)
+
   const { handleChange, handleReset, values } = useForm(initialValues)
+  const {
+    values: valuesUpdate,
+    handleChange: handleChangeUpdate,
+    handleReset: handleResetUpdate,
+    handleChangeAllValues,
+  } = useForm(initialValues)
   const query = useQuery({
     queryKey: ['getRubberRepair'],
     queryFn: getRubberRepair,
@@ -50,8 +73,62 @@ export default function RubberRepairPage() {
     },
   })
 
+  const mutationDelete = useMutation({
+    mutationFn: deleteRubberRepair,
+    mutationKey: ['deleteServicesPrices'],
+    onSuccess: () => {
+      toast({
+        title: 'Servicio eliminado',
+        description: 'El servicio se eliminó correctamente',
+      })
+      queryClient.invalidateQueries({
+        type: 'active',
+        queryKey: ['getRubberRepair'],
+      })
+      handleResetUpdate()
+      setIsOpenUpdate(false)
+    },
+    onError: () => {
+      toast({
+        title: 'Error',
+        description: 'No se pudo eliminar el servicio',
+      })
+    },
+  })
+
+  const mutationUpdate = useMutation({
+    mutationFn: updateRubberRepair,
+    mutationKey: ['updateServicesPrices'],
+    onSuccess: () => {
+      toast({
+        title: 'Servicio actualizado',
+        description: 'El servicio se actualizó correctamente',
+      })
+      queryClient.invalidateQueries({
+        type: 'active',
+        queryKey: ['getRubberRepair'],
+      })
+      handleResetUpdate()
+      setIsOpenUpdate(false)
+    },
+    onError: () => {
+      toast({
+        title: 'Error',
+        description: 'No se pudo actualizar el servicio',
+      })
+    },
+  })
+
   const handleSubmit = () => {
     mutation.mutate({ ...values })
+  }
+
+  const handleSubmitDelete = () => {
+    mutationDelete.mutate({ id: valuesUpdate.id })
+  }
+
+  const handleSubmitUpdate = () => {
+    mutationUpdate.mutate({ id: valuesUpdate.id, ...valuesUpdate })
   }
 
   return (
@@ -62,6 +139,42 @@ export default function RubberRepairPage() {
         onSubmit={handleSubmit}
         handleChange={handleChange}
       />
+      <Sheet
+        open={isOpenUpdate}
+        onOpenChange={(value) => {
+          setIsOpenUpdate(value)
+        }}
+      >
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Editar Servicio</SheetTitle>
+          </SheetHeader>
+          <div className='grid gap-4 py-4'>
+            {columnsRubberRepair.map((column) => (
+              <div
+                key={column.accessorKey}
+                className='grid grid-cols-4 items-center gap-4'
+              >
+                <Label className='text-right'>
+                  {column.header.replace('$', '')}
+                </Label>
+                <Input
+                  value={valuesUpdate[column.accessorKey]}
+                  onChange={handleChangeUpdate(column.accessorKey)}
+                  id={column.accessorKey}
+                  className='col-span-3'
+                />
+              </div>
+            ))}
+          </div>
+          <SheetFooter>
+            <Button onClick={handleSubmitUpdate}>Actualizar</Button>
+            <Button variant={'outline'} onClick={handleSubmitDelete}>
+              Eliminar
+            </Button>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
       <div className='w-full flex justify-center items-center'>
         <RotateLoader
           className='mt-10'
@@ -69,7 +182,16 @@ export default function RubberRepairPage() {
           color='#fff'
         />
       </div>
-      <DataTable columns={columnsRubberRepair} data={query.data} />
+      {!query.isFetching && (
+        <DataTable
+          onClickRow={(data) => {
+            setIsOpenUpdate(true)
+            handleChangeAllValues(data)
+          }}
+          columns={columnsRubberRepair}
+          data={query.data}
+        />
+      )}
     </Container>
   )
 }
