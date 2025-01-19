f6486f019bdad9fcb12bebecc9f7d975b5e5c21e
development-hobby <jesusalcalarojas@gmail.com>
2025-01-19 00:59:57 -0400

    feat: update rubber repair API endpoints to include '/api' prefix

--- src/services/rubber-services.ts
+++ src/services/rubber-services.ts
@@ -1,11 +1,11 @@
 export async function getRubberRepair() {
-  const response = await fetch('/rubber-repair')
+  const response = await fetch('/api/rubber-repair')
   const jsonData = await response.json()
   return jsonData.data
 }
 
 export async function saveRubberRepair(values: Record<string, string>) {
-  const response = await fetch('/rubber-repair', {
+  const response = await fetch('/api/rubber-repair', {
     method: 'POST',
     body: JSON.stringify({ ...values }),
   })
