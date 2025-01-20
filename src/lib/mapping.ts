bb20b5f25adfb0713537c34c371449b5edcb0bbc
development-hobby <jesusalcalarojas@gmail.com>
2025-01-19 21:43:17 -0400

    feat: add mapping functions for Prices and Rubber Repair services

--- src/lib/mapping.ts
+++ src/lib/mapping.ts
@@ -12,3 +12,34 @@ export const mappingPaymentsServices = (
     }
   })
 }
+
+export const mappingPricesServices = (data: Array<Record<string, string>>) => {
+  return data.map((item) => {
+    return {
+      id: item.id,
+      measure: item.measure,
+      repair: item.repair,
+      change: item.change,
+      rotation: item.rotation,
+      disassembly: item.disassembly,
+      assembly: item.assembly,
+      vulcanization: item.vulcanization,
+      fineValve: item.fineValve,
+      thickValve: item.thickValve,
+      date: item.createdAt,
+    }
+  })
+}
+
+export const mappingRubberRepair = (data: Array<Record<string, string>>) => {
+  return data.map((item) => {
+    return {
+      id: item.id,
+      description: item.description,
+      vulcanizationN: item.vulcanizationN,
+      vulcanizationG: item.vulcanizationG,
+      vulcanizationValve: item.vulcanizationValve,
+      date: item.createdAt,
+    }
+  })
+}
